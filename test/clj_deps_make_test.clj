47eb09bacf746f6692a6447e004eabcaa6e60bc5
Andrew Tropin <andrewtropin@gmail.com>
2020-03-05 16:37:07 +0300

    Add dummy functions and tests

--- test/clj_deps_make_test.clj
+++ test/clj_deps_make_test.clj
@@ -5,5 +5,26 @@
 (deftest sample-true-test
   (is (= 14 (sut/summ 6 8))))
 
+(deftest get-player-age
+  (let [player {:name "Player Name"
+                :year 1980}
+
+        player-age 40
+
+        updated-player (sut/add-player-age player)]
+    (is (contains? updated-player :age))
+    (is (= player-age (:age updated-player)))))
+
+
+(deftest test-double-endpoint
+  (let [number   123
+        request  {:method  "POST"
+                  :headers {"X-Header" "Value"}
+                  :url     "/double"
+                  :body    (str number)}
+        response (sut/double request)]
+    (is (= 200 (:status response)))
+    (is (= (str (* 2 number)) (:body response)))))
+
 (deftest additional-true-test
   (is true))
